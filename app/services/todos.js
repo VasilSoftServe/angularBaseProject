983f0c70eaab65495bfbcab334a681a60a63c864
VasilSoftServe <vasilsokolov0@gmail.com>
2016-01-22 15:59:06 +0200

    check if name less than 3 symbols

--- app/services/todos.js
+++ app/services/todos.js
@@ -4,7 +4,7 @@
     angular
         .module('app')
         .factory('todos', service); 
- 
+    var minLenghtOfListName =3;
     var id =1,
         listTodos = [],
         todos = [];
@@ -17,6 +17,10 @@
         };
 
     function addListTodos (name) {
+            if (_.size(name) <= minLenghtOfListName ){
+                return alert('This list has too short name!!!');                
+            }
+
             if (isDuplicate(name)) {
                 alert('The list existed!');
             }else{
